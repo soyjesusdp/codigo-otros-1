62a49e5a30ee786eb1465f0016fb242fd3f9d60d
soyjesusdp <jesusduertoprado@gmail.com>
2024-11-26 16:43:56 -0600

    Actualización

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,24 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
+const $n = document.querySelector('.name'); //!Se corrige a clase
+const $b = document.querySelector('.blog'); //!Se corrige a clase
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
+async function displayUser(username) {
   $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
+  const response = await fetch(`${usersEndpoint}/${username}`); //!Se añade función asíncrona ya que await depende de ello.
+  const data = await response.json(); //! Corrección de plantilla
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  $n.textContent = `${data.name}`; //!Se utilizan backtips
+  $b.textContent = `${data.blog}`; //!Se utilizan backtips
+  $l.textContent = `${data.location}`; //!Se utilizan backtips
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err}`;
 }
 
 displayUser('stolinski').catch(handleError);
+
